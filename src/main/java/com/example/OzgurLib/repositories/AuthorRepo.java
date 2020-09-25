20dd7709d9c7405ad99825ba17a8b7d036155734
Onur Talantimur <onurtalantimur@gmail.com>
2020-09-25 20:13:08 +0300

    repo fixed.

--- src/main/java/com/example/OzgurLib/repositories/AuthorRepo.java
+++ src/main/java/com/example/OzgurLib/repositories/AuthorRepo.java
@@ -1,6 +1,7 @@
 package com.example.OzgurLib.repositories;
 
+import com.example.OzgurLib.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepo extends JpaRepository<AuthorRepo,Long> {
+public interface AuthorRepo extends JpaRepository<Author,Long> {
 }
