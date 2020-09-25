82c7394178038b03ae5a5abeb4d72192ebf165a7
Onur Talantimur <onurtalantimur@gmail.com>
2020-09-25 11:30:31 +0300

    first commit

---
+++ src/main/java/com/example/OzgurLib/OzgurLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.OzgurLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzgurLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzgurLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OzgurLib/OzgurLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OzgurLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzgurLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
