4ef7072a3e1c1b2ebfc2511ddb1346c771542c10
Onur Talantimur <onurtalantimur@gmail.com>
2020-09-27 20:08:26 +0300

    for book list , model has been added

--- src/main/java/com/example/OzgurLib/controller/BookController.java
+++ src/main/java/com/example/OzgurLib/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.example.OzgurLib.controller;
 
+import com.example.OzgurLib.repositories.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/book")
 public class BookController {
 
+    @Autowired
+    private BookRepo bookRepo;
+
+
     @GetMapping("/list")
-    public String showList(){
+    public String showList(Model model){
+
+        var bookList = bookRepo.findAll();
+
+        model.addAttribute(bookList);
+
+
         return "/lists/book-list";
     }
 
