da218f34a566b2da7b53f6140df6647c1cd1fda5
Onur Talantimur <onurtalantimur@gmail.com>
2020-09-29 16:32:23 +0300

    user details added.

---
+++ src/main/java/com/example/OzgurLib/security/MyUserDetails.java
@@ -0,0 +1,68 @@
+package com.example.OzgurLib.security;
+
+import com.example.OzgurLib.entities.Role;
+import com.example.OzgurLib.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
+public class MyUserDetails implements UserDetails {
+
+    private User user;
+
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Set<Role> roleSet = user.getRoles();
+
+        ArrayList<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        roleSet.stream().map(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+
+
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
