9a15ff3e2e9c1a3cd1c6ad88cbacc17c26ba677a
Onur Talantimur <onurtalantimur@gmail.com>
2020-09-29 17:06:21 +0300

    SecurityConfig added.

---
+++ src/main/java/com/example/OzgurLib/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.OzgurLib.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsServiceImp();
+    }
+
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        return authenticationProvider;
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/list/**").hasAnyAuthority("ADMIN","USER").anyRequest().authenticated()
+                .and().formLogin().permitAll().defaultSuccessUrl("/",true);
+    }
+}
