0cd45aff69a32496ffc8fd319586dcd94867593c
Onur Talantimur <onurtalantimur@gmail.com>
2020-09-30 11:58:46 +0300

    AspectConfig added.

---
+++ src/main/java/com/example/OzgurLib/aspect/AspectConfig.java
@@ -0,0 +1,9 @@
+package com.example.OzgurLib.aspect;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AspectConfig {
+}
